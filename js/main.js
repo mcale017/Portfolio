5a055e3135f11a9994da208e5c520e1627f1a78c
mcale017 <mcale017@gmail.com>
2018-07-26 22:37:46 -0500

    minor changes such as navbar, email, etc

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 ; (function () {
-// Lines 139 to 146 are what I edited out so that the nav bar stays the same at the top
-
 	'use strict';
-
-
 
 	// iPad and iPod detection	
 	var isiPad = function () {
@@ -22,43 +18,27 @@
 		$(window).stellar();
 	};
 
-
-
 	// Burger Menu
 	var burgerMenu = function () {
-
 		$('body').on('click', '.js-fh5co-nav-toggle', function (event) {
-
 			event.preventDefault();
-
 			if ($('#navbar').is(':visible')) {
 				$(this).removeClass('active');
 			} else {
 				$(this).addClass('active');
 			}
-
-
-
 		});
-
 	};
 
-
 	var goToTop = function () {
-
 		$('.js-gotop').on('click', function (event) {
-
 			event.preventDefault();
-
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
 			}, 500);
-
 			return false;
 		});
-
 	};
-
 
 	// Page Nav
 	var clickMenu = function () {
@@ -66,50 +46,39 @@
 		$('#navbar a:not([class="external"])').click(function (event) {
 			var section = $(this).data('nav-section'),
 				navbar = $('#navbar');
-
 			if ($('[data-section="' + section + '"]').length) {
 				$('html, body').animate({
 					scrollTop: $('[data-section="' + section + '"]').offset().top
 				}, 500);
 			}
-
 			if (navbar.is(':visible')) {
 				navbar.removeClass('in');
 				navbar.attr('aria-expanded', 'false');
 				$('.js-fh5co-nav-toggle').removeClass('active');
 			}
-
 			event.preventDefault();
 			return false;
 		});
-
-
 	};
 
 	// Reflect scrolling in navigation
 	var navActive = function (section) {
-
 		var $el = $('#navbar > ul');
 		$el.find('li').removeClass('active');
 		$el.each(function () {
 			$(this).find('a[data-nav-section="' + section + '"]').closest('li').addClass('active');
 		});
-
 	};
 
 	var navigationSection = function () {
-
 		var $section = $('section[data-section]');
-
 		$section.waypoint(function (direction) {
-
 			if (direction === 'down') {
 				navActive($(this.element).data('section'));
 			}
 		}, {
 				offset: '150px'
 			});
-
 		$section.waypoint(function (direction) {
 			if (direction === 'up') {
 				navActive($(this.element).data('section'));
@@ -117,43 +86,30 @@
 		}, {
 				offset: function () { return -$(this.element).height() + 155; }
 			});
-
 	};
-
-
-
-
 
 	// Window Scroll
 	var windowScroll = function () {
 		var lastScrollTop = 0;
-
 		$(window).scroll(function (event) {
-
 			var header = $('#fh5co-header'),
 				scrlTop = $(this).scrollTop();
-
 			if (scrlTop > 500 && scrlTop <= 2000) {
 				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
 			} else if (scrlTop <= 500) {
-				/*
 				if (header.hasClass('navbar-fixed-top')) {
 					header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
 					setTimeout(function () {
 						header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
 					}, 100);
 				}
-				*/
 			}
 
 		});
 	};
 
-
-
 	// Animations
 	// Home
-
 	var homeAnimate = function () {
 		if ($('#fh5co-home').length > 0) {
 
@@ -172,93 +128,62 @@
 
 						});
 					}, 200);
-
-
 					$(this.element).addClass('animated');
-
 				}
 			}, { offset: '80%' });
-
 		}
 	};
 
 	var workAnimate = function () {
 		if ($('#fh5co-work').length > 0) {
-
 			$('#fh5co-work').waypoint(function (direction) {
-
 				if (direction === 'down' && !$(this.element).hasClass('animated')) {
-
-
 					setTimeout(function () {
 						$('#fh5co-work .to-animate').each(function (k) {
 							var el = $(this);
-
 							setTimeout(function () {
 								el.addClass('fadeInUp animated');
 							}, k * 200, 'easeInOutExpo');
-
 						});
 					}, 200);
-
-
 					$(this.element).addClass('animated');
-
 				}
 			}, { offset: '80%' });
-
 		}
 	};
 
 	var aboutAnimate = function () {
 		var about = $('#fh5co-about');
 		if (about.length > 0) {
-
 			about.waypoint(function (direction) {
-
 				if (direction === 'down' && !$(this.element).hasClass('animated')) {
-
-
 					setTimeout(function () {
 						about.find('.to-animate').each(function (k) {
 							var el = $(this);
-
 							setTimeout(function () {
 								el.addClass('fadeInUp animated');
 							}, k * 200, 'easeInOutExpo');
-
 						});
 					}, 200);
-
-
-
 					$(this.element).addClass('animated');
-
 				}
 			}, { offset: '80%' });
-
 		}
 	};
 
 	var countersAnimate = function () {
 		var counters = $('#fh5co-counters');
 		if (counters.length > 0) {
-
 			counters.waypoint(function (direction) {
-
 				if (direction === 'down' && !$(this.element).hasClass('animated')) {
-
 					var sec = counters.find('.to-animate').length,
 						sec = parseInt((sec * 200) + 400);
-
 					setTimeout(function () {
 						counters.find('.to-animate').each(function (k) {
 							var el = $(this);
-
 							setTimeout(function () {
 								el.addClass('fadeInUp animated');
 							}, k * 200, 'easeInOutExpo');
-
 						});
 					}, 200);
 
@@ -280,61 +205,39 @@
 
 						});
 					}, sec);
-
-
-
-
-
 					$(this.element).addClass('animated');
-
 				}
 			}, { offset: '80%' });
-
 		}
 	};
 
 	var contactAnimate = function () {
 		var contact = $('#fh5co-contact');
 		if (contact.length > 0) {
-
 			contact.waypoint(function (direction) {
-
 				if (direction === 'down' && !$(this.element).hasClass('animated')) {
-
 					setTimeout(function () {
 						contact.find('.to-animate').each(function (k) {
 							var el = $(this);
-
 							setTimeout(function () {
 								el.addClass('fadeInUp animated');
 							}, k * 200, 'easeInOutExpo');
-
 						});
 					}, 200);
-
 					$(this.element).addClass('animated');
-
 				}
 			}, { offset: '80%' });
-
 		}
 	};
 
 	// Document on load.
 	$(function () {
-
 		parallax();
-
 		burgerMenu();
-
 		clickMenu();
-
 		windowScroll();
-
 		navigationSection();
-
 		goToTop();
-
 
 		// Animations
 		homeAnimate();
@@ -343,5 +246,4 @@
 		aboutAnimate();
 		contactAnimate();
 	});
-
 }());
